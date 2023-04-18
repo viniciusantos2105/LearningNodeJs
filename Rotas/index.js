707c5a1380a1099362cc414cd9884abc86f756c6
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-04-18 19:28:28 -0300

    trabalhando com parametros em rotas

--- Rotas/index.js
+++ Rotas/index.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app")
+    res.send("Seja bem-vindo ao meu app");
 })
 
 app.get("/sobre", function(req, res){
-    res.send("Minha pagina é sobre")
+    res.send("Minha pagina é sobre");
+})
+
+app.get("/soma/:num1/:num2", function(req, res){
+    numberOne = parseInt(req.params.num1);
+    numberTwo = parseInt(req.params.num2);
+    soma = numberOne + numberTwo;
+    res.send(soma);
 })
 
 
 app.listen(8081, function(){
-    console.log("Servidor rodando na porta 8081")
+    console.log("Servidor rodando na porta 8081");
 });
