be4546c9768942d1969fe7cb7e6ce543519e2560
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-04-19 08:17:00 -0300

    Criando models e adicionado creates

--- Rotas/test.js
+++ Rotas/test.js
@@ -9,3 +9,41 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
     console.log("Falha ao se conectar: " + erro);
 });
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.create({
+    titulo: "titulo",
+    conteudo: "conteudo dsadasdada"
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+Usuario.create({
+    nome: "Vini",
+    sobrenome: "Santos",
+    idade: 10,
+    email: "email@email.com"
+})
+
+//Usuario.sync({force: true}) = comando para criar tables
