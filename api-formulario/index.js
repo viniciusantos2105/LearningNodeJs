9c2cad1b62c49e39e4bdf34cb5256d37e23ac861
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-04-20 08:04:11 -0300

    aplicando body parser para recuperar campos do formulario

--- api-formulario/index.js
+++ api-formulario/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars')
 var handle = handlebars.create({defaultLayout: 'main'});
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 
 // Config
     // Template Engine
     app.engine('handlebars', handle.engine);
     app.set('view engine', 'handlebars');
-
+    //Body Parser
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
     //Conexão com o banco de dados MySQL
     const sequelize = new Sequelize('test', 'root', '1234', {
         host: "localhost",
@@ -17,9 +20,13 @@ const Sequelize = require('sequelize');
    
 //Rotas
     app.get('/cad', function(req, res){
-         res.render('formulario')
+         res.render('formulario');
     })
 
+    app.post('/add', function(req, res){
+        req.body.conteudo
+        res.send("Texto: "+ req.body.titulo +  "\r\nConteudo: " + req.body.conteudo);
+    })
 
 app.listen(8081, function(){
     console.log("Servidor rodando na porta 8081");
