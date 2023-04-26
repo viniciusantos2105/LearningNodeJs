1dd00a67138cade3fa10b4b1d07850308f5c7c5c
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-04-26 11:03:30 -0300

    Registrando usuario no banco de dados

--- TrabalhandoComMongo/aprendendo.js
+++ TrabalhandoComMongo/aprendendo.js
@@ -11,4 +11,43 @@ mongoose.connect("mongodb://127.0.0.1:27017/aprendendo?directConnection=true&ser
     console.log("Houve um erro ao se conectar ao mongoDB: "+ err)
 })
 
-//mongodb://127.0.0.1:27017/aprendendo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.1
+//Definindo Model
+const UserSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+})
+
+//Collection
+mongoose.model('usuarios', UserSchema)
+
+//Registrando usuario no banco
+const newUser = mongoose.model('usuarios')
+
+    new newUser({
+        nome: "Vinícius",
+        sobrenome: "Jesus",
+        email: "vini@email.com",
+        idade: 21,
+        pais: "Brasil"
+    }).save().then(() => {
+        console.log("Usuário criado com sucesso!")
+    }).catch((err) => {
+        console.log("Ocorreu um erro ao registrar o usuario no banco!!")
+    })
